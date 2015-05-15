80bd1913e623291e6418305c3b555a682366b09d
Tarun Kumar <tarun14691@gmail.com>
2015-05-15 11:54:45 +0530

    Initial boiler-plate.

---
+++ SpringJPA/src/main/java/com/test/FirstRun.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Package :- com.test
+ * Created by tarunk
+ * Created on 5/14/15.
+ */
+public class FirstRun {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
